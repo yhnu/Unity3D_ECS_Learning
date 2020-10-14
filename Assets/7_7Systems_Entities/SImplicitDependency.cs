1510a9a95735d19616b60d7801120f2b475b90aa
happyyi <immb@qq.com>
2020-10-14 15:23:42 +0800

    7_7 Dependency Demo

---
+++ Assets/7_7Systems_Entities/SImplicitDependency.cs
@@ -0,0 +1,56 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+[DisableAutoCreation]  //测试时需要禁用掉
+public class SImplicitDependency : SystemBase
+{
+    //protected override void OnCreate()
+    //{
+    //
+    //}
+
+    protected override void OnUpdate()
+    {
+        Entities.ForEach((ref CJobAData d1) =>
+        {
+            for(int i=0; i < 3000; i++)
+            {
+                float c = math.sin(i);
+            }
+        })
+        .WithName("ForEach_Job_1")
+        .Schedule();
+
+        Entities.ForEach((ref CJobBData d2) =>
+        {
+            for (int i = 0; i < 3000; i++)
+            {
+                float c = math.sin(i);
+            }
+        })
+        .WithName("ForEach_Job_2")
+        .Schedule();
+
+        Entities.ForEach((ref CJobCData d3) =>
+        {
+            for (int i = 0; i < 3000; i++)
+            {
+                float c = math.sin(i);
+            }
+        })
+        .WithName("ForEach_Job_3")
+        .Schedule();        
+    }
+}
+
+/**
+ * 隐式序列依赖
+ * 
+ * The function schedules three jobs, each depending on the previous one
+ * 
+ * 
+ */
---
+++ Assets/7_7Systems_Entities/SManualDependency.cs
@@ -0,0 +1,64 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+//[DisableAutoCreation]
+public class SManualDependency : SystemBase
+{
+    //protected override void OnCreate()
+    //{
+    //
+    //}
+
+    protected override void OnUpdate()
+    {
+        JobHandle One = Entities.ForEach((ref CJobAData d1) =>
+        {
+            for (int i = 0; i < 3000; i++)
+            {
+                float c = math.sin(i);
+            }
+        })
+        .WithName("ForEach_Job_1")
+        .Schedule(this.Dependency); //Schedule 需要传入对应的依赖,才会返回Job Handle
+
+        JobHandle Two = Entities.ForEach((ref CJobBData d2) =>
+        {
+            for (int i = 0; i < 3000; i++)
+            {
+                float c = math.sin(i);
+            }
+        })
+        .WithName("ForEach_Job_2") 
+        .Schedule(this.Dependency); //Schedule 需要传入对应的依赖,才会返回Job Handle
+
+
+        JobHandle intermediateDependencies = JobHandle.CombineDependencies(One, Two);
+
+        NativeArray<int> result = new NativeArray<int>(1, Allocator.TempJob);
+
+        JobHandle finalDependency = Job
+            .WithName("Job_Three")
+            .WithDisposeOnCompletion(result)
+            .WithCode(() =>
+            {
+                /*...*/
+                result[0] = 1;
+            })
+            .Schedule(intermediateDependencies);
+
+        this.Dependency = finalDependency;
+
+    }
+}
+
+/**
+ * 隐式序列依赖
+ * 
+ * The function schedules three jobs, each depending on the previous one
+ * 
+ * 
+ */
