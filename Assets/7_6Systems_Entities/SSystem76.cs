e003915b4d83ca97a0498af13126ee3545635c19
happyyi <immb@qq.com>
2020-10-14 15:01:27 +0800

    7_6 WithReadOnly

--- Assets/7_6Systems_Entities/SSystem76.cs
+++ Assets/7_6Systems_Entities/SSystem76.cs
@@ -5,7 +5,7 @@ using Unity.Jobs;
 using Unity.Mathematics;
 using Unity.Transforms;
 
-[DisableAutoCreation]
+//[DisableAutoCreation]
 public class SSystem76 : SystemBase
 {
     //protected override void OnCreate()
@@ -52,26 +52,41 @@ public class SSystem76 : SystemBase
         */
 
         /*
-        NativeArray<float> sumArray = new NativeArray<float>(count, Allocator.TempJob);
+        NativeArray<float> sumArray = new NativeArray<float>(1, Allocator.TempJob);
 
         Entities.WithReadOnly(typeof(CData76))
             .ForEach((int entityInQueryIndex, in DynamicBuffer<CData76> data) => {
             
                 for(int i=0;i < data.Length; i++)
                 {
-                    sumArray[i] += data[i].Value;
+                    sumArray[0] += data[i].Value;
                 }            
-            // Implement the work to perform for each entity here.
-            // You should only access data that is local or that is a
-            // field on this job. Note that the 'rotation' parameter is
-            // marked as 'in', which means it cannot be modified,
-            // but allows this job to run in parallel with other jobs
-            // that want to read Rotation component data.
-            // For example,
-            //     translation.Value += math.mul(rotation.Value, new float3(0, 0, 1)) * deltaTime;
         }).WithName("OnUpdate")
-        .Schedule();
+        .Schedule();        
         */
+
+        NativeArray<float> itemArray = new NativeArray<float>(10, Allocator.Persistent);
+        for(int i=0; i < 10; i++)
+        {
+            itemArray[i] = i;
+        }
+
+        NativeArray<float> sum = new NativeArray<float>(1, Allocator.Persistent);
+
+        Entities
+            .WithReadOnly(itemArray)                //针对局部变量访问,通过WithReadOnly,来加速job计算
+            //.WithReadOnly(sum)                       //注意:这地方开启会报错
+            .WithStoreEntityQueryInField(ref query)
+            .ForEach((int entityInQueryIndex, in DynamicBuffer<CData76> data) =>
+            {
+                for(int i=0; i< itemArray.Length; i++)
+                {
+                    sum[0] += itemArray[i];
+                }                
+            }).WithName("OnUpdate")
+            .WithDisposeOnCompletion(sum)
+            .WithDisposeOnCompletion(itemArray)
+            .Schedule();            
     }
 }
 
@@ -86,6 +101,10 @@ public class SSystem76 : SystemBase
  * When you execute the function using a job (by calling one of the Schedule functions instead of Run) there are some restrictions on the captured variables and how you use them:
    
     1. Only native containers and blittable types can be captured. 只支持值类型
-    2. A job can only write to captured variables that are native containers. (To “return” a single value, create a native array with one element.) 如果你向写入数据,需要使用native containers
+    2. A job can only write to captured variables that are native containers. (To “return” a single value, create a native array with one element.) 
+       如果你向写入数据,需要使用native containers
+ * 
+ * //NativeArrray的申请和释放问题
+ * 
  * 
  */
