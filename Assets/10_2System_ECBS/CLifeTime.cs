87060a98986aea6a5c371ed50331b53f40e8558f
happyyi <immb@qq.com>
2020-10-19 10:14:05 +0800

    10_2 ECBS 示例| 学习文档

---
+++ Assets/10_2System_ECBS/CLifeTime.cs
@@ -0,0 +1,19 @@
+﻿using Unity.Entities;
+using Unity.Mathematics;
+
+[GenerateAuthoringComponent]
+public struct CLifeTime : IComponentData
+{
+    // Add fields to your component here. Remember that:
+    //
+    // * The purpose of this class is to store data for authoring purposes - it is not for use while the game is
+    //   running.
+    // 
+    // * Traditional Unity serialization rules apply: fields must be public or marked with [SerializeField], and
+    //   must be one of the supported types.
+    //
+    // For example,
+    //    public float scale;
+
+    public long Value;
+}
---
+++ Assets/10_2System_ECBS/SLifetimeSystem.cs
@@ -0,0 +1,54 @@
+﻿using Unity.Burst;
+using Unity.Collections;
+using Unity.Entities;
+using Unity.Jobs;
+using Unity.Mathematics;
+using Unity.Transforms;
+
+//[DisableAutoCreation]
+public class SLifetimeSystem : SystemBase
+{
+    EndSimulationEntityCommandBufferSystem m_EndSimulationEcbSystem;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        m_EndSimulationEcbSystem = World.GetExistingSystem<EndSimulationEntityCommandBufferSystem>();
+    
+    }
+
+    protected override void OnUpdate()
+    {
+        // Assign values to local variables captured in your job here, so that it has
+        // everything it needs to do its work when it runs later.
+        // For example,
+        //     float deltaTime = Time.DeltaTime;
+
+        // This declares a new kind of job, which is a unit of work to do.
+        // The job is declared as an Entities.ForEach with the target components as parameters,
+        // meaning it will process all entities in the world that have both
+        // Translation and Rotation components. Change it to process the component
+        // types you want.
+
+        var ecb = m_EndSimulationEcbSystem.CreateCommandBuffer().AsParallelWriter();        
+
+
+
+        Entities.ForEach((Entity entity, int entityInQueryIndex, ref CLifeTime lifetime) =>
+        {
+            if (lifetime.Value == 0)
+            {
+                ecb.DestroyEntity(entityInQueryIndex, entity);
+            }
+            else
+            {
+                lifetime.Value -= 1;
+            }
+
+        }).WithName("OnUpdate")
+        .ScheduleParallel();
+
+        m_EndSimulationEcbSystem.AddJobHandleForProducer(this.Dependency);
+    }
+}
